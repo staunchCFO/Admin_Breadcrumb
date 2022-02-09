b067571ce6a8340d2e30046013edc1984048c493
Emeka Okezie <okezieemeka949@gmail.com>
2022-02-09 01:36:37 +0100

    Redux setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import './bootstrap.min.css';
 import {GlobalStyles} from './global_styles'
-import App from './App';
+import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
+const store = configureStore();
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
-    <>
-        <GlobalStyles />
-        <App />
-    </>,
-    document.getElementById('root')
-)
+	<Provider store={store}>
+		<PersistGate persistor={persistor}>
+            <GlobalStyles />
+			<App />
+		</PersistGate>
+	</Provider>,
+	document.getElementById("root")
+);
